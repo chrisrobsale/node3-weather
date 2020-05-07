0468e8a433a53e2289bf59d664b00cf20dd39a4b
Salenga <christian.p.salenga@accenture.com>
2020-05-07 10:23:04 +0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,13 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     messageOne.textContent='Fetching...'
     messageTwo.textContent=''
 
-    fetch('http://localhost:3000/weather?address='+location).then((res) => {
+    fetch('/weather?address='+location).then((res) => {
         res.json().then((data) => {
             if(data.error){
                 console.log(data.error)
